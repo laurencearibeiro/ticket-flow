a4bb101eaa733b0032fe06de7f20e8be09b6c86b
laurenceribeiro <laurencesci@gmail.com>
2026-09-05 12:36:47 -0300

    Initial Commit

---
+++ TicketFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicketFlow
+//
+//  Created by Laurence Alves Ribeiro on 05/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicketFlow/TicketFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketFlowApp.swift
+//  TicketFlow
+//
+//  Created by Laurence Alves Ribeiro on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TicketFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
